4681501c58c9f39db14cc02ecd4db440c03743a1
mmgreiner <mmgreiner@bluewin.ch>
2023-08-03 10:32:46 +0200

    added excel

---
+++ excel/excel.go
@@ -0,0 +1,31 @@
+package excel
+
+import (
+	"github.com/xuri/excelize/v2"
+)
+
+// reads the given excel file and converts each row into a slice of strings.
+// uses the 'rowser' function to convert this slice into the actual data struct.
+func ReadExcel[T any](fn string, header bool, rower func([]string) T) []T {
+	file, err := excelize.OpenFile(fn)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	sheet := file.GetSheetName(0)
+	rows, _ := file.Rows(sheet)
+
+	if header {
+		// skip header line
+		rows.Next()
+	}
+
+	data := []T{}
+	for rows.Next() {
+		row, _ := rows.Columns()
+		dataRow := rower(row)
+		data = append(data, dataRow)
+	}
+	return data
+}
